74dee776fbaffacfdb4df99d4007a56f4ee28bc6
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-02-10 22:13:45 +0530

    Create ReadCSVUsingDataFrame.scala
    reading csv file using dataframe code

---
+++ src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
@@ -0,0 +1,12 @@
+package com.sparkbyexamples.spark.dataframe
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+
+object ReadCSVUsingDataFrame {
+
+  val spark:SparkSession = SparkSession.builder().master("local[5]").appName("sparkscalabysab").getOrCreate()
+
+  // To do
+  
+  }
+}
