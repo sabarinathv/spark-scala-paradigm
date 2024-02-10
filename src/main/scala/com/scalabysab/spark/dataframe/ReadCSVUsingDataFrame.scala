1082a5e8555f7d9e6a75f21fea7d5c439c717c69
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-02-10 22:14:56 +0530

    Update ReadCSVUsingDataFrame.scala
    adding notes

--- src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
@@ -1,9 +1,10 @@
-package com.sparkbyexamples.spark.dataframe
+package com.scalabysab.spark.dataframe
 
-import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.SparkSession
 
 object ReadCSVUsingDataFrame {
 
+  // Creating SparkSession
   val spark:SparkSession = SparkSession.builder().master("local[5]").appName("sparkscalabysab").getOrCreate()
 
   // To do
