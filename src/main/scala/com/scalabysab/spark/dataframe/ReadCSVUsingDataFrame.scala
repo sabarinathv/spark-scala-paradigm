46ab025c24b72dfdb8fab07bf1da81492005e52c
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-02-13 11:28:21 +0530

    Updating Reading CSV file using Dataframe
    Updating Reading CSV file using Dataframe

--- src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
@@ -7,6 +7,11 @@ object ReadCSVUsingDataFrame {
   // Creating SparkSession
   val spark:SparkSession = SparkSession.builder().master("local[5]").appName("sparkscalabysab").getOrCreate()
 
+  // Reading from CSV file
+  val df = spark.read.csv("src/main/resources/student_records.csv")
+  df.show()
+  df.printSchema()
+  
   // To do
   
   }
