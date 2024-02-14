b3d8bfd8508e039da0e23ec494c9a45a4cb20eab
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-02-14 17:30:49 +0530

    Adding dataframe options
    Adding dataframe options

--- src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ReadCSVUsingDataFrame.scala
@@ -8,9 +8,14 @@ object ReadCSVUsingDataFrame {
   val spark:SparkSession = SparkSession.builder().master("local[5]").appName("sparkscalabysab").getOrCreate()
 
   // Reading from CSV file
-  val df = spark.read.csv("src/main/resources/student_records.csv")
-  df.show()
-  df.printSchema()
+  val df1 = spark.read.csv("src/main/resources/student_records_with_header.csv")
+  df1.show()
+  df1.printSchema()
+
+  // Reading from CSV file with options
+  val df2 = spark.read.options(Map("inferSchema"->"true","delimiter"->",","header"->"true")).csv("src/main/resources/student_records_with_header.csv")
+  df2.show()
+  df2.printSchema()
   
   // To do
   
