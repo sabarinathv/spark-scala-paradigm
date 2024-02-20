03028287c52fb45ff37150d9f4bdd0a0db512b77
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-02-20 21:25:51 +0530

    Converting parquet to csv code
    Converting parquet to csv code added

--- src/main/scala/com/scalabysab/spark/dataframe/ConvertParquetToCsv.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ConvertParquetToCsv.scala
@@ -1,17 +1,17 @@
 package com.scalabysab.spark.dataframe
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{SaveMode, SparkSession}
 
   object ConvertParquetToCsv extends App {
 
     // Creating spark session
     val spark: SparkSession = SparkSession.builder().master("local[3]").appName("scalabysab").getOrCreate()
 
-    // Reading parquet file
-    val df = spark.read.format("parquet").load("path_to_parquet_file/students.parquet")
+    // Reading Parquet file
+    val df = spark.read.format("parquet").load("path_to_parquet_file/students_details.parquet")
     df.show()
-    df.printSchema()
-
-    // To do
-
+    
+    // Converting Parquet file to Csv
+    df.write.mode(SaveMode.Overwrite).csv("/tmp/output/students_details.csv")
+    
   }
