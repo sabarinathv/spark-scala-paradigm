daa4d1e60878045f9cc8a28e5ee3bd4fcb57c23c
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-02-20 17:39:49 +0530

    Update ConvertParquetToCsv.scala
    Update ConvertParquetToCsv.scala

--- src/main/scala/com/scalabysab/spark/dataframe/ConvertParquetToCsv.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ConvertParquetToCsv.scala
@@ -7,6 +7,11 @@ import org.apache.spark.sql.SparkSession
     // Creating spark session
     val spark: SparkSession = SparkSession.builder().master("local[3]").appName("scalabysab").getOrCreate()
 
+    // Reading parquet file
+    val df = spark.read.format("parquet").load("path_to_parquet_file/students.parquet")
+    df.show()
+    df.printSchema()
+
     // To do
 
   }
