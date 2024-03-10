cb35c9a8af0fe0777389092df655be55ca7d5846
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-03-10 19:46:27 +0530

    Adding code for reading text file using Dataset
    Adding code for reading text file using Dataset

--- src/main/scala/com/scalabysab/spark/dataframe/ReadTextFileUsingDataframe.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ReadTextFileUsingDataframe.scala
@@ -8,10 +8,15 @@ object ReadTextFileUsingDataframe {
   val spark:SparkSession = SparkSession.builder().master("local[3]").appName("sparkscalabysab").getOrCreate()
 
   // Reading from text file
-  val df = spark.read.text("src/main/resources/sample1.txt")
-  df.show()
-  df.printSchema()
+  val dataFrame = spark.read.text("src/main/resources/text/sample1.txt")
+  ddataFramef.show()
+  dataFrame.printSchema()
 
+  // Reading from text file using Spark Dataset
+  val dataSet:Dataset[String] = spark.read.textFile("src/main/resources/text/sample1.txt")
+  dataSet.show()
+  dataSet.printSchema()
+  
   // To do
   
   }
