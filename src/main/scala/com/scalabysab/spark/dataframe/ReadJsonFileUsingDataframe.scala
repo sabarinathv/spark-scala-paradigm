07825f94ad4d26e579f351abc54673bea988994e
Sabarinath Velayudham <sanath_ind@yahoo.co.in>
2024-03-24 20:20:56 +0530

    Update ReadJsonFileUsingDataframe.scala

--- src/main/scala/com/scalabysab/spark/dataframe/ReadJsonFileUsingDataframe.scala
+++ src/main/scala/com/scalabysab/spark/dataframe/ReadJsonFileUsingDataframe.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.SparkSession
 object ReadJsonFileUsingDataframe {
 
   // Creating SparkSession
-  val spark:SparkSession = SparkSession.builder().master("local[1]").appName("sparkscalabysab").getOrCreate()
+  val spark:SparkSession = SparkSession.builder().master("local[3]").appName("sparkscalabysab").getOrCreate()
  
   // To do
   
